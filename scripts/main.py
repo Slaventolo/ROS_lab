7beec16c2c71a6a9621b400b0f3e4b8e66c339cf
Slava <email@email.com>
2022-11-13 04:23:31 -0800

    3

--- scripts/main.py
+++ scripts/main.py
@@ -1,43 +1,35 @@
+
 #!/usr/bin/env python
 
 import rospy
-from glassbridge.srv import myService
+
+from std_msgs.msg import String
 
 class player:
     def down(self):
-        print('game over')
-        self.flag = True
+        self.publisher.publish('finish')
+        if self.length<18:
+            print('game over')
+        else:
+            print('Congratulations')
     def __init__(self):
-        self.sequence = ''
+        self.length = 1
         self.flag = False
+        self.subsriber = rospy.Subscriber('answerTopic', String, self.callback)
+        self.publisher = rospy.Publisher('shutdownTopic', String, queue_size = 10) 
         rospy.init_node('main')
         rospy.wait_for_service('myService')
         rospy.on_shutdown(self.down)
-        try:
-            self.s = rospy.ServiceProxy('myService', myService)
+        rospy.spin()
 
-        except rospy.ServiceException:
-            print('failed to connect to to service')
-    def start(self):
-        while len(self.sequence)<18:
-            if self.flag:
-                break
-            self.Input()
-        if not self.flag:
-            print('Congratulations')
-    def Input(self):
-        letter = ''
-        while letter != 'l' and letter != 'r':
-            letter = input().lower()
-            if letter !='l' and letter !='r':
-                print('Wrong input')
-        response = self.s(letter, self.sequence)
-        if response.answer == 'dead':
+
+    def callback(self, data):
+        if data.data == 'dead':
             rospy.signal_shutdown('dead')
-        else:
-            self.sequence+=letter
+        elif data.data == 'move next':
             print('Move next')
+        else:
+            rospy.signal_shutdown('finish')
 if __name__ == '__main__':
     print('Welcome to the game. Use l or r letters to play')
     player = player()
-    player.start()
--- scripts/service.py
+++ scripts/service.py
@@ -1,28 +1,47 @@
 #!/usr/bin/env python
 from random import randint
 import rospy
-
+from std_msgs.msg import String
 from glassbridge.srv import myService, myServiceResponse
 
 class service:
-
     def generateSequence(self):
         mask = {0:'l', 1:'r'}
         array = [mask[randint(0,1)] for i in range(18)]
         print(array)
         return ''.join(array)
+        
+    def on_shutdown(self):
+        print('game ended')
+      
     def __init__(self):
         self.sequence = self.generateSequence()
         self.s = rospy.Service('myService', myService, self.callback)
         rospy.init_node('service')
+        self.position = 0 
+        self.publisher = rospy.Publisher('answerTopic', String, queue_size=10)
+        self.subsriber = rospy.Subscriber('shutdownTopic', String, self.answer) 
+        rospy.on_shutdown(self.on_shutdown)
         rospy.spin()
+        
+    def answer(self, data):
+        rospy.signal_shutdown('dead')
+        
     def callback(self, req):
-
-        position = len(req.sequence)
-        if self.sequence[position] == req.nextLetter:
-            return myServiceResponse('move next')
+        if self.sequence[self.position] == req.nextLetter:
+            self.position+=1
+            if self.position == 18:
+                self.position = 0
+                self.publisher.publish('win')
+                return myServiceResponse('win')
+            else:
+                self.publisher.publish('move next')
+                return myServiceResponse('move next')
         else:
-            
+            self.position+=1
+            self.publisher.publish('dead')
             return myServiceResponse('dead')
+            
+            
 if __name__ == '__main__':
     service = service()
